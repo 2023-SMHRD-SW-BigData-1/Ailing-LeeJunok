51dec5a19759116cb065977a667a4a5e55cdf382
Pumpkin980423 <tngh5352@gmail.com>
2023-07-05 14:49:52 +0900

    0705오후커밋

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes');
+const path = require('path');
+const cors = require('cors');
+
+
+
+app.set('port',process.env.PORT||8090);
+
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static(path.join(__dirname,'react-project/build')));
+app.use('/',indexRouter)
+
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'번 port에서 서버연결 대기중...');
+})
