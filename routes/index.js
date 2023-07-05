f253f1244fc4d0948e5d589481115bbf9c89dc45
jeongsoi <injjang007@naver.com>
2023-07-05 16:09:08 +0900

    바보야

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const oracleDbConfig = require('../config/oracleDatabase'); 
+
+router.get('/test', async (req, res) => {
+  let sql = 'SELECT PROD_IMG FROM T_PRODUCT WHERE PROD_SEQ = 1';
+
+  try {
+    const connection = await oracleDbConfig.init();
+    
+    console.log('연결 성공!'); // 연결 성공 메시지 출력
+
+    const results = await connection.execute(sql);
+    res.json(results.rows);
+
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } catch (err) {
+    console.error(`연결 실패: ${err}`);
+    res.status(500).send('Internal Server Error');
+  }
+});
