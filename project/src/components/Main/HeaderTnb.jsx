5cdc48d37cb4db113655a00799552f254eb31d37
Pumpkin980423 <tngh5352@gmail.com>
2023-07-05 09:21:41 +0900

    0705오전커밋

--- project/src/components/Main/HeaderTnb.jsx
+++ project/src/components/Main/HeaderTnb.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HeaderTnb = () => {
     return (
@@ -7,7 +8,7 @@ const HeaderTnb = () => {
         <ul>
             <li><a href="#">로그인</a></li>
             <li><a href="#">회원가입</a></li>
-            <li><a href="#">주문조회</a></li>
+            <li><Link to='/cart'>주문조회</Link></li>
             <li><a href="#">고객센터</a></li>
             <li><a href="#">마이페이지</a></li>
             <li><a href="#">장바구니</a></li>
