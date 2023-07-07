b949a997d7e63511c110309cafaa3c3557225839
정준옥 <wnsdhr7221@gmail.com>
2023-07-07 18:34:02 +0900

    login

--- project/src/index.js
+++ project/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import App from './App';
-import App from './components/Login/Login01';
+import App from './components/Login/Login05';
 
 
 
