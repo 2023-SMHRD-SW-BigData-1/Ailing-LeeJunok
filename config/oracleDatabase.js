5ff7689f0c8500c43edcbf705a7f7a6c66aae9d8
jeongsoi <injjang007@naver.com>
2023-07-07 18:00:19 +0900

    1eks

--- config/oracleDatabase.js
+++ config/oracleDatabase.js
@@ -9,30 +9,20 @@ router.use(express.urlencoded({extended:false}))
 const app = express()
 oracledb.initOracleClient({libDir:'C:/Users/smhrd/Desktop/oracleClient'})
 
-// Oracle DB 서버와 연결할 프로필 정보 설정
 const dbConfig = {
-    user : "campus_h_230627_2",
-    password : "smhrd2",
-    connectString : 'project-db-stu2.smhrd.com'
+user : "campus_h_230627_2",
+password : "smhrd2",
+connectString : 'project-db-stu2.smhrd.com'
 }
-
-// 미들웨어 설정(JSON을 파싱하는 미들웨어를 사용함)
-app.use(express.json());
-
-router.get('/product', async(req, res)=>{
+router.get('/', async(req, res)=>{
     let connection;
     let result;
     try{
         connection = await oracledb.getConnection(dbConfig)
-        result = await connection.execute("select JSON_OBJECT('prod_img', prod_img) FROM t_product");
+        result = await connection.execute('select prod_img from T_PRODUCT');
         console.log(result.rows);
-
-        // 응답 추가
-        res.json(result.rows);
     } catch (error) {
         console.log(error);
-        // 에러에 대한 응답 추가하기
-        res.status(500).send(`Error: ${error.message}`);
     } finally {
         if (connection){
             try {
