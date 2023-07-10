76152ed26ad785ff64c8237dee79d8ed28c03058
leejooooooon <dlwns7293@naver.com>
2023-07-10 17:45:55 +0900

    이준

--- project/src/index.js
+++ project/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import App from './App';
-import App from './components/Payment/Payment01';
+import App from './App';
 
 
 
