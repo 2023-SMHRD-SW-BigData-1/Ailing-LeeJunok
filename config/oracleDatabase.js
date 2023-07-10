361315284030baecbd34125b0d57695ef5a9da2f
jeongsoi <injjang007@naver.com>
2023-07-10 10:40:31 +0900

    yaml 추가

--- config/oracleDatabase.js
+++ config/oracleDatabase.js
@@ -7,32 +7,53 @@ const router = express.Router()
 router.use(express.json())
 router.use(express.urlencoded({extended:false}))
 const app = express()
+const yaml = require('js-yaml')
 oracledb.initOracleClient({libDir:'C:/Users/smhrd/Desktop/oracleClient'})
 
 const dbConfig = {
-user : "campus_h_230627_2",
-password : "smhrd2",
-connectString : 'project-db-stu2.smhrd.com'
+    user: "campus_h_230627_2",
+    password: "smhrd2",
+    connectString: 'project-db-stu2.smhrd.com'
 }
-router.get('/', async(req, res)=>{
+
+router.get('/select', async (req, res) => {
     let connection;
-    let result;
-    try{
-        connection = await oracledb.getConnection(dbConfig)
-        result = await connection.execute('select prod_img from T_PRODUCT');
+
+    try {
+        connection = await oracledb.getConnection(dbConfig);
+        const result = await connection.execute('select prod_img from T_PRODUCT');
         console.log(result.rows);
+
+        const products = result.rows.map(row => ({ prod_img: row[0] }));
+
+        const yamlData = yaml.dump({ products });
+
+        res.header("Content-Type", "text/yaml");
+        res.send(yamlData); 
     } catch (error) {
         console.log(error);
+        res.status(500).send("Server error");
     } finally {
-        if (connection){
+        if (connection) {
             try {
-                await connection.close()
-            } catch (error){
+                await connection.close();
+            } catch (error) {
                 console.log(error);
             }
         }
     }
-    console.log(result.rows);
-})
+});
+
+
+// router.post('/insert', (req, res) => {
+//     let { id, pw, nick } = req.body
+
+//     let sql = 'insert into t_user values (?,?,?)'
+//     conn.query(sql, [id, pw, nick], function (err, rows, fields) {
+//         console.log(rows);
+//         res.redirect('/select')
+//     })
+
+// })
 
 module.exports = router;
