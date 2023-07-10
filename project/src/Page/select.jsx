a4f3baa2c504c032ec44baf9f613a0f09d58ceb2
jeongsoi <injjang007@naver.com>
2023-07-10 11:20:41 +0900

    일단

--- project/src/Page/select.jsx
+++ project/src/Page/select.jsx
@@ -1,3 +1,32 @@
-import ('/Users/smhrd/Desktop/mainproject/Ailing-LeeJunok/config/oracleDatabase')
+import oracledb from '/Users/smhrd/Desktop/mainproject/Ailing-LeeJunok/config/oracleDatabase';
 
-<h1></h1>
+import React, { useState, useEffect } from 'react';
+import oracledb from '/Users/smhrd/Desktop/mainproject/Ailing-LeeJunok/config/oracleDatabase';
+
+const Product = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('/product');
+            const yamlData = await response.text();
+            const data = yaml.safeLoad(yamlData);
+            setProducts(data.products);
+        };
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map((product, index) => (
+                    <li key={index}>
+                        <img src={product.prod_img}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
