9bb195e0a8ddb29ef965d65dad2c9066969fc0e8
Pumpkin980423 <tngh5352@gmail.com>
2023-07-11 17:15:59 +0900

    0711 제발제발

--- project/src/components/Main/ProductCard.jsx
+++ project/src/components/Main/ProductCard.jsx
@@ -2,10 +2,16 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const ProductCard = ({url,name}) => {
+const ProductCard = ({url,name,price,category,maker}) => {
     return (
     
-    <Link className='innercard'><img src={url} alt="준비중"/><p>{name}</p></Link>
+    <Link >
+    <img src={url} alt="준비중"/>
+    <p className='name'><strong>{name}</strong></p>
+    <p className='maker'>{'('+maker+')'}</p>
+    <span className='categorytext'>{category}</span>
+    <p className='price'>{price+' 원'}</p>
+    </Link>
     
     )
 }
