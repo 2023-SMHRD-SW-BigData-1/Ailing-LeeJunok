9f985208a36539d990a83dc3ab994c8090cba379
jeongsoi <injjang007@naver.com>
2023-07-11 15:35:00 +0900

    이미지 교환

--- project/src/Json/Product.js
+++ project/src/Json/Product.js
@@ -5,7 +5,7 @@ var productjs =
         "PROD_NAME": "비타민C\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 10400,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300003/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/M835NJGD69DN/v3/product20-아이엠퓨어비타민C.png",
         "PROD_CATEGORY": "피로, 피부"
     },
     {
@@ -13,7 +13,7 @@ var productjs =
         "PROD_NAME": "남성 복합 비타민",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 15100,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300001/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/3FUNACALVSG7/v2/product01-아이엠비지맨.png",
         "PROD_CATEGORY": "면역, 남성건강"
     },
     {
@@ -21,7 +21,7 @@ var productjs =
         "PROD_NAME": "칼슘\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 13000,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300004/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/6UV22UZJDA6C/v2/product06-아이엠본레시피.png",
         "PROD_CATEGORY": "관절, 면역, 수면"
     },
     {
@@ -29,7 +29,7 @@ var productjs =
         "PROD_NAME": "마그네슘\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 12200,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300005/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/6YSP9NH9Q454/v3/product04-아이엠마그네슘.png",
         "PROD_CATEGORY": "간건강,뼈"
     },
     {
@@ -37,7 +37,7 @@ var productjs =
         "PROD_NAME": "아연&미네랄\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 8900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300006/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/RHTKW59H5KNN/v2/product12-아이엠아연이뮨키퍼.png",
         "PROD_CATEGORY": "남성건강, 눈, 장"
     },
     {
@@ -45,7 +45,7 @@ var productjs =
         "PROD_NAME": "MSM\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 11500,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300007/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/8JSE2RS52HLG/v2/product16-아이엠조인트서포터즈.png",
         "PROD_CATEGORY": "관절"
     },
     {
@@ -53,7 +53,7 @@ var productjs =
         "PROD_NAME": "멀티비타민&미네랄",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 19300,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300008/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/CAALDK2MVRSQ/v1/(클레이풀)product03-아이엠더원.png",
         "PROD_CATEGORY": "간건강, 다이어트, 면역, 스트레스, 피로"
     },
     {
@@ -61,7 +61,7 @@ var productjs =
         "PROD_NAME": "밀크씨슬\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 16900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300009/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/NCC8V9WBCMHQ/v2/product05-아이엠밀크씨슬.png",
         "PROD_CATEGORY": "간건강"
     },
     {
@@ -69,7 +69,7 @@ var productjs =
         "PROD_NAME": "테아닌",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 13600,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300010/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/EZE8PGM3ZGQU/v2/product02-아이엠라일락페어리.png",
         "PROD_CATEGORY": "수면, 스트레스"
     },
     {
@@ -77,7 +77,7 @@ var productjs =
         "PROD_NAME": "프로바이오틱스",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 22900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300012/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/6GEJR89ZJCZ3/v2/product21-아이엠프로바이오틱스.png",
         "PROD_CATEGORY": "장"
     },
     {
@@ -85,7 +85,7 @@ var productjs =
         "PROD_NAME": "루테인 ",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 16900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300014/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/6PW6H27HARB9/v2/product13-아이엠아이즈포뮬러.png",
         "PROD_CATEGORY": "눈"
     },
     {
@@ -93,7 +93,7 @@ var productjs =
         "PROD_NAME": "비타민D\t",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 8800,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2103050000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/MDMPDCCSZAHK/v1/product22-아이엠비타민D.png",
         "PROD_CATEGORY": "뼈"
     },
     {
@@ -101,7 +101,7 @@ var productjs =
         "PROD_NAME": "잔티젠",
         "PROD_MAKER": "노바렉스",
         "PROD_PRICE": 29800,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2107080000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/8BLFWXV65S87/v1/product24-아이엠잔티젠.png",
         "PROD_CATEGORY": "다이어트"
     },
     {
@@ -109,7 +109,7 @@ var productjs =
         "PROD_NAME": "TG 오메가3",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 17800,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2203250000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/LRUBGLQ86HCD/v1/product14-아이엠알티지오메가3.png",
         "PROD_CATEGORY": "관절, 혈액"
     },
     {
@@ -117,7 +117,7 @@ var productjs =
         "PROD_NAME": "비타민A D3 E",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 19900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2208100000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/NJWSBBVPC8J8/v1/product008-아이엠비타민ADE.png",
         "PROD_CATEGORY": "눈, 장, 피부"
     },
     {
@@ -125,7 +125,7 @@ var productjs =
         "PROD_NAME": "코엔자임Q10",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 19900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2208100001/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/3JU4U2P4D2MX/v1/product017-아이엠코엔자임Q10.png",
         "PROD_CATEGORY": "항산화, 피로"
     },
     {
@@ -133,7 +133,7 @@ var productjs =
         "PROD_NAME": "쏘팔메토 옥타코사놀",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 23100,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2208100002/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/P6KVFCDRUEFN/v1/product018-아이엠파이어맨.png",
         "PROD_CATEGORY": "남성건강"
     },
     {
@@ -141,7 +141,7 @@ var productjs =
         "PROD_NAME": "히알루론산",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 21200,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2208100003/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/XP443JVBHAV3/v1/product30-아이엠스킨촉촉히알루론산.png",
         "PROD_CATEGORY": "여성건강, 피부"
     },
     {
@@ -149,7 +149,7 @@ var productjs =
         "PROD_NAME": "감마리놀렌산",
         "PROD_MAKER": "노바렉스",
         "PROD_PRICE": 32600,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2209270000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/KH4RFUTNYSVE/v1/poduct11-울트라제2의봄.png",
         "PROD_CATEGORY": "피부, 면역, 혈액"
     },
     {
@@ -157,7 +157,7 @@ var productjs =
         "PROD_NAME": "중성 비타민C",
         "PROD_MAKER": "노바렉스",
         "PROD_PRICE": 16000,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2209270001/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/H2NMMEVHDBX8/v1/product15-2-울트라비타민1000.png",
         "PROD_CATEGORY": "피로, 피부"
     },
     {
@@ -165,7 +165,7 @@ var productjs =
         "PROD_NAME": "프로바이오틱스(100억)",
         "PROD_MAKER": "노바렉스",
         "PROD_PRICE": 25800,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2210150000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/P9MKPD3DR58M/v1/product25-바이탈락토패밀리.png",
         "PROD_CATEGORY": "장"
     },
     {
@@ -173,7 +173,7 @@ var productjs =
         "PROD_NAME": "모나콜린K",
         "PROD_MAKER": "닥터엘리자베스",
         "PROD_PRICE": 10900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2303220000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/QTD65DMNBYTY/v1/clayful_catalog_모나콜린K.png",
         "PROD_CATEGORY": "혈액, 콜레스테롤"
     },
     {
@@ -181,7 +181,7 @@ var productjs =
         "PROD_NAME": "오메가3 큰캡슐",
         "PROD_MAKER": "JW중외제약",
         "PROD_PRICE": 32900,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2304040000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/JS3ZMTCJHWR7/v1/product28-알티지오메가3맥스.png",
         "PROD_CATEGORY": "관절, 혈액"
     },
     {
@@ -189,7 +189,7 @@ var productjs =
         "PROD_NAME": "여성 복합 비타민",
         "PROD_MAKER": "콜마비앤에이치",
         "PROD_PRICE": 14500,
-        "PROD_IMG": "https://cdn.monolabs.io/products/FM2010300000/main_01.png",
+        "PROD_IMG": "https://cdn.clayful.io/stores/SDTSPBE344H5.2QCZ4SCL2GQ8/images/9CWWRPHPLY9D/v2/product07-아이엠비지우먼.png",
         "PROD_CATEGORY": "면역, 여성건강"
     },
     {
@@ -197,7 +197,7 @@ var productjs =
         "PROD_NAME": "로사빈",
         "PROD_MAKER": "유바생명과학",
         "PROD_PRICE": 55000,
-        "PROD_IMG": "https://www.nightoclock.co.kr/data/goods/85/2023/01/_temp_16732450725259view.JPG",
+        "PROD_IMG": "_16732450725259view.JPG",
         "PROD_CATEGORY": "스트레스, 피로"
     }
 ]
