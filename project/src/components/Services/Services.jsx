cdb70ae1b5dbca229627f08773f141ba5418139e
jeongsoi <injjang007@naver.com>
2023-07-11 17:15:56 +0900

    이단

---
+++ project/src/components/Services/Services.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const services = () => {
+    return (
+        <div>
+            <div>
+                <div>고객센터</div>
+                <div>
+                    <div>
+                        <a href="/services">공지사항</a>
+                    </div>
+                    <div>
+                        <a href="">보도자료</a>
+                    </div>
+                    <div>
+                        <a href="">자주하는 질문</a>
+                    </div>
+                    <div>
+                        <a href="">1:1 문의</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>공지사항</div>
+                    <div>
+                        <div>1</div>
+                        <div>배송일정</div>
+                        <div>2023.07.11</div>
+                        <div>0</div>
+                    </div>
+                    <div>
+                        <div>2</div>
+                        <div>서비스 오픈</div>
+                        <div>2023.00.00</div>
+                        <div>0</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default services;
