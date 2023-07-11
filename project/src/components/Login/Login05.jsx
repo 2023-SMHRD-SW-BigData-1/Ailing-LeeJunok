9eee2945d69ca83c35c462082a5b5a6e2f752c8a
정준옥 <wnsdhr7221@gmail.com>
2023-07-11 19:57:30 +0900

    Footer

--- project/src/components/Login/Login05.jsx
+++ project/src/components/Login/Login05.jsx
@@ -14,11 +14,11 @@ const Join = () => {
             <div> 회원가입 </div>
 
         <div>
-            <div>이름 입력(*)</div>
+            <div id = "USER_NAME">이름 입력(*)</div>
             <input type ="text" className="w-full border-2 border-black"
             placeholder="이름 입력"/>
 
-            <div>ID 입력(*)</div>
+            <div id = "USER_ID">ID 입력(*)</div>
             <input type ="email" className="w-full border-10 border-black"
             placeholder="이메일 입력"/>
      
@@ -29,6 +29,10 @@ const Join = () => {
             <div>PW 확인(*)</div>
             <input type ="password" className="w-full border-2 border-black"
             placeholder="PW 확인"/>
+
+            <div id = "USER_NAME">전화번호</div>
+            <input type ="text" className="w-full border-2 border-black"
+            placeholder="전화"/>
 
             <div> 생년월일 </div>
             <input type ="date" className="w-full border-2 border-black"/>
