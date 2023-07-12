452f7eb0f13072f219a642c3d9430b34d86b9983
leejooooooon <dlwns7293@naver.com>
2023-07-12 16:47:32 +0900

    0712

--- project/src/Page/Login/Login05.jsx
+++ project/src/Page/Login/Login05.jsx
@@ -1,15 +1,93 @@
 
 // ===============================================
 // import { Checkbox } from '@mui/material';
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../../css/Join/Join.css'
+import ReactDom from 'react-dom';
+import DaumPostcode from "react-daum-postcode";
 
+
+
+
+
+const PopupDom = ({ children }) => {
+  const el = document.getElementById('popupDom');
+  return ReactDom.createPortal(children, el);
+};
+
+const PopupPostCode = React.forwardRef((props) => {
+	// 우편번호 검색 후 주소 클릭 시 실행될 함수, data callback 용
+    const handlePostCode = (data) => {
+        let fullAddress = data.address;
+        let extraAddress = ''; 
+        
+        if (data.addressType === 'R') {
+          if (data.bname !== '') {
+            extraAddress += data.bname;
+          }
+          if (data.buildingName !== '') {
+            extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+          }
+          fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+
+        // 실제 데이터 부분
+        console.log(data)
+        console.log(fullAddress) // 실제주소 : fullAddress = data.address
+        console.log(data.zonecode) // 우편번호
+
+
+        props.postRef.current.value = data.zonecode
+        props.addRef.current.value = fullAddress
+
+
+        props.onClose()
+    }
+ 
+    const postCodeStyle = {
+        display: "block",
+        
+        top: "10%",
+        width: "600px",
+        height: "600px",
+        padding: "7px"
+      };
+ 
+    return(
+        <div>
+            <DaumPostcode style={postCodeStyle} onComplete={handlePostCode} />
+            <button type='button' onClick={() => {props.onClose()}} className='postCode_btn'>닫기</button>
+        </div>
+    )
+})
+
+
+// =============================================================================
 const Join = () => {
+	// 팝업창 상태 관리
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+ 
+	// 팝업창 열기
+    const openPostCode = () => {
+        setIsPopupOpen(true)
+    }
+ 
+	// 팝업창 닫기
+    const closePostCode = () => {
+        setIsPopupOpen(false)
+    }
+    const postRef = useRef()
+    const addRef = useRef()
+
+
+
+
   return (
+    
     <>
         <div className='mainSec'><h2>회원가입</h2></div>
         <div className="joinContainer">
-    <form action="doJoin" method="POST" className='joinForm' onsubmit="DoJoinForm__submit(this); return false;">
+    <form action="doJoin" method="POST" className='joinForm' >
       <div className="textForm">
         <input name="loginId" type="text" className="id" placeholder="아이디">
         </input>
@@ -29,8 +107,22 @@ const Join = () => {
       <div className="textForm">
         <input name="cellphoneNo" type="number" className="cellphoneNo" placeholder="전화번호"/>
       </div>
+        <input value='우편번호 찾기' name="mailNumber" type="button" className="mailNumber" placeholder="우편번호 찾기" onClick={openPostCode}/>
+        <div id='popupDom'>
+                {isPopupOpen && (
+                    <PopupDom>
+                        <PopupPostCode onClose={closePostCode} postRef={postRef} addRef={addRef} />
+                    </PopupDom>
+                )}
+        </div>
       <div className="textForm">
-        <input name="address" type="address" className="address" placeholder="주소"/>
+        <input name="faddress" type="text" className="faddress" placeholder="우편번호" ref={postRef} readOnly/>
+      </div>
+      <div className="textForm">
+        <input name="address" type="text" className="address" placeholder="주소" ref={addRef} readOnly/>
+      </div>
+      <div className="textForm">
+        <input name="detailAddress" type="text" className="detailAddress" placeholder="상세주소"/>
       </div>
       <div className="textForm">
         <input name="birth" type="date" className="birth" placeholder="생년월일"/>
@@ -48,64 +140,9 @@ const Join = () => {
     </div>
     </>
     
+    
      )
     }
 
     export default Join
-
-
-
-    //------------------------------------------------
-    {/* <div>
-        <form
-            onSubmit={(e)=>{
-            e.preventDefault();
-             // alert('실행')
-            }}
-            className='flex flex-col w-60 items-start p-4'
-        > 
-            <table width="400px">
-
-        <div className='joinTable'>
-            <div id = "USER_NAME">이름 입력(*)</div>
-            <input type ="text" className="w-full border-2 border-black"
-            placeholder="이름 입력"/>
-
-            <div id = "USER_ID">ID 입력(*)</div>
-            <input type ="email" className="w-full border-10 border-black"
-            placeholder="이메일 입력"/>
-            
-            <div>PW 입력(*)</div>
-            <input type ="password" className="w-full border-2 border-black"
-            placeholder="PW 입력"/>
-
-            <div>PW 확인(*)</div>
-            <input type ="password" className="w-full border-2 border-black"
-            placeholder="PW 확인"/>
-
-            <div id = "USER_NAME">전화번호</div>
-            <input type ="text" className="w-full border-2 border-black"
-            placeholder="전화"/>
-
-            <div> 생년월일 </div>
-            <input type ="date" className="w-full border-2 border-black"/>
-
-            <div> 성별 </div>
-            <div>
-            <Checkbox name='gender' value='male'></Checkbox>남<Checkbox name='gender' value='female'></Checkbox>여
-            </div>
-            
-            <div> 주소 </div>
-            <input type ="address"/>
-            <br/>
-            <button type="submit" className="btn btn-sm ml-auto mt-4">회원가입</button>
-            
-            <button type="submit" className="btn btn-sm ml-auto mt-4">가입취소</button>
-        </div>
-
-
-            </table>
-            </form>
-        </div> */}
-
         
