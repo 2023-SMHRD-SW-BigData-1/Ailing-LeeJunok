ba14ecbc62426ba6f88cc637b8484ba05ff7aa30
jeongsoi <injjang007@naver.com>
2023-07-12 13:47:57 +0900

    하아

--- project/src/Page/ProductBox.jsx
+++ project/src/Page/ProductBox.jsx
@@ -1,48 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import '../css/ProductBox/ProductList.css';
+import React, { useState } from 'react';
 import ProductInfo from '../components/ProductList/Productinfo';
-import axios from 'axios';
+import productjs from '../Json/Product';
 
 const ProductBox = () => {
-  const initialProductCount = 9; // 초기에 노출될 제품 수
+  const initialProductCount = 9; // number of products to be initially exposed
   const [productCount, setProductCount] = useState(initialProductCount);
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/products'); // 제품 데이터를 검색하는 API 엔드포인트로 교체하세요
-        setProducts(response.data);
-      } catch (error) {
-        console.error('제품 데이터를 가져오는 중 오류 발생:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const products = productjs.map((product) => ({
+    imageSrc: product.PROD_IMG,
+    subtitle: product.PROD_CATEGORY,
+    title: product.PROD_NAME,
+    price: product.PROD_PRICE
+  }));
 
   const handleShowMore = () => {
-    setProductCount((prevCount) => prevCount + 9); // 이전에 노출된 제품 수에 9개를 추가합니다
+    setProductCount((prevCount) => prevCount + 9); // Add 9 items from the number of previously exposed products
   };
+
+
 
   const showMoreButton =
     productCount < products.length ? (
       <div className='watch'>
-        <button onClick={handleShowMore}>더 보기</button>
+      <button onClick={handleShowMore}>더 보기</button>
       </div>
-    ) : null;
+      ) : null;
 
   const productRows = [];
-  for (let i = 0; i < productCount; i++) {
+  for (let i = 0; i < productCount; i ++) {
     const rowProducts = products.slice(i, i + 1);
     productRows.push(
       <div className="grid-row" key={i}>
         {rowProducts.map((product, index) => (
           <div className="grid-item" key={index}>
             <ProductInfo
-              imageSrc={product.prodImg}
-              subtitle={product.prodCategory}
-              title={product.prodName}
-              price={product.prodPrice}
+              imageSrc={product.imageSrc}
+              subtitle={product.subtitle}
+              title={product.title}
+              price={product.price}
             />
           </div>
         ))}
@@ -52,11 +47,11 @@ const ProductBox = () => {
 
   return (
     <>
-      <div className='mainSec'><h2>제품 목록</h2></div>
-      <div className='contentBox'>
-        <div className="grid-container">{productRows}</div>
-        {showMoreButton}
-      </div>
+    <div className='mainSec'><h2>제품 리스트</h2></div>
+    <div className='contentBox'>
+      <div className="grid-container">{productRows}</div>
+      {showMoreButton}
+    </div>
     </>
   );
 };
